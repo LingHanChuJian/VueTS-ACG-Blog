cd5c08daf425b46973860a8ae4573d5f3d11aad0
linghanchujian <2739069093@qq.com>
2019-11-19 00:53:48 +0800

    feat: 增加Menu标签

---
+++ src/components/menu/Menu.vue
@@ -0,0 +1,26 @@
+<template lang="pug">
+    ul(:class="classes")
+</template>
+
+<script lang="ts">
+import { oneOf } from '@/utils'
+import { Component, Prop, Vue } from 'vue-property-decorator'
+
+@Component
+export default class Menu extends Vue {
+    @Prop({
+        default: 'vertical',
+        type: String,
+        validator(value: string) {
+            return oneOf(value, ['vertical', 'horizontal'])
+        },
+    })
+    private mode!: string
+
+    private prefixCls: string = 'menu'
+}
+</script>
+
+<style lang="stylus" scoped>
+
+</style>
