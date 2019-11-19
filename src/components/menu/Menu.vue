7038254e966c0d84b68e50af80eedf5b370eca5a
linghanchujian <2739069093@qq.com>
2019-11-20 00:52:22 +0800

    style: 更改meny 样式

--- src/components/menu/Menu.vue
+++ src/components/menu/Menu.vue
@@ -1,10 +1,13 @@
 <template lang="pug">
-    ul(:class="classes")
+    ul(:class="classes" :style="styles")
+        slot
 </template>
 
 <script lang="ts">
 import { oneOf } from '@/utils'
+import { WrapClasses, CSSStyles } from '@/types/components'
 import { Component, Prop, Vue } from 'vue-property-decorator'
+
 
 @Component
 export default class Menu extends Vue {
@@ -17,7 +20,28 @@ export default class Menu extends Vue {
     })
     private mode!: string
 
+    @Prop({ default: 250, type: [Number, String] })
+    private width!: number | string
+
+    @Prop({ type: [Number, String] })
+    private activeName?: number | string
+
     private prefixCls: string = 'menu'
+
+    private get classes(): Array<string | WrapClasses> {
+        return [
+            `${this.prefixCls}`,
+            `${this.prefixCls}-${this.mode}`,
+        ]
+    }
+
+    private get styles(): CSSStyles<CSSStyleDeclaration> {
+        const style: CSSStyles<CSSStyleDeclaration>  = {}
+        if (this.mode === 'vertical') {
+            style.width =  `${this.width}px`
+        }
+        return style
+    }
 }
 </script>
 
