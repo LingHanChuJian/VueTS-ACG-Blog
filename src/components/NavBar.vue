a5bd8a5811350f709c32b655bf5208a15cee5d0f
linghanchujian <2739069093@qq.com>
2019-12-17 02:24:31 +0800

    update: 写菜单

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,6 +1,9 @@
 <template lang="pug">
     nav.navbar
-        div.logo
+        div.navbar-left
+            div.icon-menu
+            div.logo
+        div.navbar-right
 </template>
 
 <script lang="ts">
@@ -17,3 +20,45 @@ import { Component, Vue } from 'vue-property-decorator'
 export default class NavBar extends Vue {
 }
 </script>
+
+<style lang="stylus" scoped>
+menu()
+    transition all .2s
+    background-color #000000
+    width 50px
+    height 3px  
+
+@media screen and (max-width 860px)
+    .navbar
+        width 100%
+        height 100%
+        .icon-menu
+            menu()
+            position relative
+            &:before
+                menu()
+                content ''
+                position absolute
+                top -8px
+            &:after
+                menu()
+                content ''
+                position absolute
+                bottom 8px
+        .icon-menu-opened
+            menu()
+            position relative
+            background-color transparent
+            &:before
+                menu()
+                content ''
+                position absolute
+                transform rotate(-45deg)
+                top 0
+            &:after
+                menu()
+                content ''
+                position absolute
+                transform rotate(45deg)
+                bottom 0
+</style>
