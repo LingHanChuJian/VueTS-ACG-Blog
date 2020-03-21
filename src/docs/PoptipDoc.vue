7f9d11c14cd49bead86c7d038e556aacad3032f9
linghanchujian <2739069093@qq.com>
2020-03-21 09:36:24 +0800

    update: 更新piptip文档

--- src/docs/PoptipDoc.vue
+++ src/docs/PoptipDoc.vue
@@ -1,11 +1,33 @@
 <template lang="pug">
-    
+    div
+        Anchor(title="Poptip 气泡提示")
+        Example.simple(title="基础用法")
+            template(slot="dome")
+            template(slot="description")
+            template(slot="code")
+        Example.simple(title="位置")
+            template(slot="dome")
+            template(slot="description")
+            template(slot="code")
 </template>
 
 <script lang="ts">
+import { Table } from '@/components/table'
+import { Anchor } from '@/components/anchor'
+import { Poptip } from '@/components/poptip'
+import { Column, Row } from '@/types/components'
+import { Example, Block } from '@/components/example'
 import { Component, Vue } from 'vue-property-decorator'
 
-@Component
+@Component({
+    components: {
+        Table,
+        Anchor,
+        Example,
+        Block,
+        Poptip,
+    },
+})
 export default class PoptipDoc extends Vue {
 
 }
