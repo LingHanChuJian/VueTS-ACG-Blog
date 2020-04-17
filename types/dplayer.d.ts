4ae9d0bd61290efba550fde387d35dfe5b2dbd86
Linghanchujian <2739069093@qq.com>
2020-04-17 18:51:05 +0800

    Update dplayer.d.ts

--- types/dplayer.d.ts
+++ types/dplayer.d.ts
@@ -12,7 +12,7 @@ export type DPlayerEvents = 'abort' | 'canplay' | 'canplaythrough' | 'durationch
 | 'seeked' | 'seeking' | 'stalled' | 'suspend' | 'timeupdate' | 'volumechange' | 'waiting' | 'screenshot' | 'thumbnails_show'
 | 'thumbnails_hide' | 'danmaku_show' | 'danmaku_hide' | 'danmaku_clear' | 'danmaku_loaded' | 'danmaku_send' | 'danmaku_opacity'
 | 'contextmenu_show' | 'contextmenu_hide' | 'notice_show' | 'notice_hide' | 'quality_start' | 'quality_end' | 'destroy' | 'resize'
-| 'fullscreen' | 'fullscreen_cancel' | 'subtitle_show' | 'subtitle_hide' | 'subtitle_changes'
+| 'fullscreen' | 'fullscreen_cancel' | 'subtitle_show' | 'subtitle_hide' | 'subtitle_change'
 
 export interface DPlayerOptions {
   [key: string]: any
