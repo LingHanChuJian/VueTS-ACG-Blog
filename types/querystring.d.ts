3ebc00a7933b84a8627eed37ace59ba362261b62
Linghanchujian <2739069093@qq.com>
2020-05-27 11:26:22 +0800

    Create querystring.d.ts

---
+++ types/querystring.d.ts
@@ -0,0 +1,20 @@
+declare module 'querystring' {
+    interface StringifyOptions {
+        encodeURIComponent?: (str: string) => string
+    }
+
+    interface ParseOptions {
+        maxKeys?: number
+        decodeURIComponent?: (str: string) => string
+    }
+
+    interface ParsedUrlQuery {
+        [key: string]: string | string[]
+    }
+
+    function stringify(obj?: object, sep?: string, eq?: string, options?: StringifyOptions): string
+    function parse(str: string, sep?: string, eq?: string, options?: ParseOptions): ParsedUrlQuery
+
+    function escape(str: string): string
+    function unescape(str: string): string
+}
