e3db99893c91cc643d0b40f78379612865d91461
linghanchujian <2739069093@qq.com>
2020-06-10 20:26:09 +0800

    update: dplayer 注释

--- src/utils/dplayer.ts
+++ src/utils/dplayer.ts
@@ -1,5 +1,19 @@
+/**
+ * markdown dplayer 语法设计
+ * 栗子:
+ * <div
+ * data-dplayer
+ * data-autoplay="false"
+ * data-preload="auto"
+ * data-theme="#5895be"
+ * data-type="songs 或者 song"
+ * data-song="base64(fixed + id)"
+ * data-loop="false"
+ * data-order="list"
+ * data-listmaxheight="349px"></div>
+ */
 import {} from 'dplayer'
 
-export const createDplayer = () => {
+export const createDplayer = (el: HTMLElement) => {
     // 创建
 }
