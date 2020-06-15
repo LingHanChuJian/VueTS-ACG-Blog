b1d98153c3a7328e8b9dbab996934c6893ab56fe
Linghanchujian <2739069093@qq.com>
2020-06-15 18:18:18 +0800

    Update api.d.ts

--- src/types/api.d.ts
+++ src/types/api.d.ts
@@ -10,7 +10,13 @@ export interface VideoData {
     video: string
 }
 
-export interface ResponseError {
+export interface ResponseBase {
     status: number
     message: string
+}
+
+export type ResponseError = ResponseBase
+
+export interface ResponseSuccess extends ResponseBase {
+    data: any
 }
